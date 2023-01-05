66cd76f401ea40c512934796ff4196715e0d843e
yezhimincxvxb <395995486@qq.com>
2023-01-05 21:00:51 +0800

    sc

---
+++ consumer-order-ribbon/src/main/java/com/yzm/ribbon/RibbonApplication.java
@@ -0,0 +1,25 @@
+package com.yzm.ribbon;
+
+import com.yzm.rule.MySelfRule;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+//@RibbonClient(name = "PROVIDER-PAYMENT-SERVICE", configuration = MySelfRule.class) // 注释后恢复默认轮询策略
+public class RibbonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RibbonApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
