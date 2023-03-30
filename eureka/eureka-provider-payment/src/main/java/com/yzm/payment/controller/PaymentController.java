4006425e7d1349429f86ff022b2d504ca898d5e6
yezhimincxvxb <395995486@qq.com>
2023-03-30 22:28:43 +0800

    sc

--- eureka/eureka-provider-payment/src/main/java/com/yzm/payment/controller/PaymentController.java
+++ eureka/eureka-provider-payment/src/main/java/com/yzm/payment/controller/PaymentController.java
@@ -6,11 +6,8 @@ import com.yzm.payment.service.IPaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.UUID;
 
 /**
